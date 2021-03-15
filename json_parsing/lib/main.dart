749519689d544c468338ddf22941e0f9fe0633e7
manar <manar.hani.hossni@gmail.com>
2021-03-15 18:25:01 +0200

    parsing_json

---
+++ json_parsing/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:json_parsing/parsing_json/json_parsing.dart';
+import 'package:json_parsing/parsing_json/json_parsing_map.dart';
+
+//void main() => runApp(ScaffoldExample());
+
+void main() => runApp(new MaterialApp(
+  home: JsonParsingMap(),
+));
+
+
+
+
+
+
